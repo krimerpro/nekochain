27728e79e0a0e75d9832ee40157e9c4ce602038c
varanustoken <varanustoken@gmail.com>
2023-04-29 10:02:05 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,11 +13,11 @@
               <p>Our company specializes on Blockchain Technologies,<br> Smart Contracts, dApps Development and<br> Digital Currencies Investments</p>
               <ul class="btns">
                 <li><a class="gradient theme-btn" href="https://t.me/NekoChainPortal" title="">JOIN US</a></li>
-                <li><a class="theme-btn blank" href="https://pancakeswap.finance/swap?outputCurrency=0xD05506A42f56cb66b00880fC96c32aD47634493D" title="">BUY NEKO TOKEN</a></li>
+                <li><a class="theme-btn blank" href="https://pancakeswap.finance/swap?outputCurrency=0X" title="">BUY NEKO TOKEN</a></li>
               </ul>
 
               <div class="row">&nbsp;</div>
-              <span style="font-weight:bold;color:white;padding-top:20px">Contract Address : 0xD05506A42f56cb66b00880fC96c32aD47634493D</span>
+              <span style="font-weight:bold;color:white;padding-top:20px">Contract Address : 0X</span>
             </div>
             <div class="fimg animute wow fadeIn" data-wow-delay="0.2s">
               <img src="{{ asset('images/resource/mockup1.png') }}" alt="" />
@@ -129,7 +129,7 @@
     </div>
   </div>
 </section>
-
+<!-- 
 <section>
   <div class="block">
     <div class="container">
@@ -150,7 +150,7 @@
       </div>
     </div>
   </div>
-</section>
+</section> -->
 
 <section id="three">
   <div class="block">
