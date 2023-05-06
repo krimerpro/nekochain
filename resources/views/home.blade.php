bf9a3ee1fb38a7e2fbc709b68202c77396d9e09b
varanustoken <varanustoken@gmail.com>
2023-05-06 15:50:10 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,11 +13,11 @@
               <p>Our company specializes on Blockchain Technologies,<br> Smart Contracts, dApps Development and<br> Digital Currencies Investments</p>
               <ul class="btns">
                 <li><a class="gradient theme-btn" href="https://t.me/NekoChainPortal" title="">JOIN US</a></li>
-                <li><a class="theme-btn blank" href="https://pancakeswap.finance/swap?outputCurrency=0x" title="">BUY NEKO TOKEN</a></li>
+                <li><a class="theme-btn blank" href="https://pancakeswap.finance/swap?outputCurrency=0x5F1468501BA70036a769EE6ffd8D8b8b37c230Bc" title="">BUY NEKO TOKEN</a></li>
               </ul>
 
               <div class="row">&nbsp;</div>
-              <span style="font-weight:bold;color:white;padding-top:20px">Contract Address : 0x</span>
+              <span style="font-weight:bold;color:white;padding-top:20px">Contract Address : 0x5F1468501BA70036a769EE6ffd8D8b8b37c230Bc</span>
             </div>
             <div class="fimg animute wow fadeIn" data-wow-delay="0.2s">
               <img src="{{ asset('images/resource/mockup1.png') }}" alt="" />
